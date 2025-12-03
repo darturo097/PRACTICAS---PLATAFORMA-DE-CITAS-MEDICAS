73828a106ca6f7194dba6eb248555ed2afb61ece
darturo097 <diego.arteaga.isw@unipolidgo.edu.mx>
2025-12-03 10:25:03 -0600

    Update api.php

--- Plataforma/EnviarMensajes/api.php
+++ Plataforma/EnviarMensajes/api.php
@@ -23,7 +23,7 @@ try {
     $mail->isSMTP();
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPAuth = true;
-    $mail->Username = "diego.arteaga.097@gmail.com"; 
+    $mail->Username = 
     $mail->Password = 
     $mail->SMTPSecure = "tls";
     $mail->Port = 587;
